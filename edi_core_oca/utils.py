f57814c7e0ea00051413b81d1f9778a428ae885b
DavidJForgeFlow <david.jimenez@forgeflow.com>
2025-11-27 10:07:54 +0100

    [REF] edi_oca: remove checksum
    When trying to migrate from v15.0 and the full filestore, the checksum is literally stoping the server as more than 90 GB of data is being encoded. Removing this to be able to migrate.

--- edi_core_oca/utils.py
+++ edi_core_oca/utils.py
@@ -2,7 +2,6 @@
 # @author Simone Orsi <simahawk@gmail.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import hashlib
 from collections.abc import Callable
 from dataclasses import dataclass
 from functools import wraps
@@ -45,7 +44,3 @@ class EDIExchangeActionResult:
 def normalize_string(cls, a_string, sep="_"):
     """Normalize given string, replace dashes with given separator."""
     return cls.env["ir.http"]._slugify(a_string).replace("-", sep)
-
-
-def get_checksum(filecontent):
-    return hashlib.md5(filecontent).hexdigest()

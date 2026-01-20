camptocamp/edi

edi_oca: improve auto btns
* auto btns on/off flag per exchange type
* offer hook to customize how btns are rendered
* improve UI

edi_oca/models/edi_exchange_type.py
@@ -115,6 +115,11 @@ class EDIExchangeType(models.Model):
         It will be executed if variable result has been defined as True
         """,
     )
+    model_manual_btn = fields.Boolean(
+        string="Manual button on form",
+        help="Automatically display a btn on related models' form."
+        # TODO: "Button settings can be configured via advanced settings."
+    )
 
     _sql_constraints = [
         (

ForgeFlow/edi-framework

edi_oca/models/edi_exchange_type.py
@@ -115,6 +115,11 @@ class EDIExchangeType(models.Model):
         It will be executed if variable result has been defined as True
         """,
     )
+    model_manual_btn = fields.Boolean(
+        string="Manual button on form",
+        help="Automatically display a button on related models' form."
+        # TODO: "Button settings can be configured via advanced settings."
+    )
 
     _sql_constraints = [
         (